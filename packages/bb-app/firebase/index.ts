f14d772f10cb04526f8d758df17756ba7cb3acf7
Afoejoe <afoejoe@gmail.com>
2022-01-04 22:11:54 +0300

    initial commit

---
+++ packages/bb-app/firebase/index.ts
@@ -0,0 +1,27 @@
+import {
+	collection,
+	doc,
+	getDoc,
+	getDocs,
+	getFirestore,
+	query,
+} from 'firebase/firestore';
+import { getApps, initializeApp } from 'firebase/app';
+
+import { getAnalytics } from 'firebase/analytics';
+
+let app;
+if (!getApps()?.length) {
+	app = initializeApp({
+		apiKey: process.env.FIREBASE_API_KEY,
+		authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+		projectId: process.env.FIREBASE_PROJECT_ID,
+		storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+		messagingSenderId: process.env.FIREBASE_MESSENGER_SENDER_ID,
+		appId: process.env.FIREBASE_APP_ID,
+	});
+}
+const analytics = getAnalytics(app);
+const db = getFirestore();
+export { db, collection, getDocs, getDoc, doc, query };
+export default app;
